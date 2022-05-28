3230a8050450f6a003a44a431bcec3f47ff05b5c
Rohimin_TI07 <93963688+iminonlineshop@users.noreply.github.com>
2022-05-28 23:37:38 +0700

    Update index.js

--- src/components/ui/Button/index.js
+++ src/components/ui/Button/index.js
@@ -20,17 +20,17 @@ const Button = styled.button`
     `}
 
 // Props size
-    ${(props) => props.size == sm && css`
+    ${(props) => props.size === sm && css`
         font-size: 0.8rem;
         padding: 0.2rem 0.5rem;
     `}
 
-    ${(props) => props.size == md && css`
+    ${(props) => props.size === md && css`
         font-size: 1rem;
         padding: 0.5rem 1rem;
     `}
 
-    ${(props) => props.size == lg && css`
+    ${(props) => props.size === lg && css`
         font-size: 1.3rem;
         padding: 0.5rem 1rem;
     `}
